c5892b4e27edc194bf806433e0a6946124bee58e
Devyon Lozano <devyonlozano@gmail.com>
2025-04-13 18:57:54 -0600

    feat: complete main layout, style and font choice

--- ticker.js
+++ ticker.js
@@ -1,15 +1,17 @@
+const numFormat = new Intl.NumberFormat('en-US', { 
+    minimumFractionDigits: 4 
+});
+
 setInterval(()=>{
     let startTimeStamp = Date.UTC(2025, 3, 11, 17, 0, 0, 0);
     let currentTimeStamp = Date.now();
     let targetTimeStamp = Date.UTC(2025, 3, 28, 0, 0, 0, 0);
 
-    console.log(startTimeStamp, currentTimeStamp,targetTimeStamp)
-
     let percentage = ((currentTimeStamp-startTimeStamp)/(targetTimeStamp-startTimeStamp)) * 100
     percentage = Math.round(percentage*10000)/10000
 
-    console.log(percentage)
-
-    let percentElement = document.getElementById("percentage");
-    percentElement.textContent = percentage.toString();
+    for (let percentElement of document.getElementsByClassName("percentage-num")) {    
+        percentElement.textContent = numFormat.format(percentage);
+        console.log(percentElement.textContent)
+    }
 }, 500)
